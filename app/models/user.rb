be4625abe45191ad9e3630e28f366b1dcd592a73
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-02-16 16:05:47 +0900

    #4 Add user_session

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,5 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   validates :email, uniqueness: true, presence: true
-  validates :first_name, presence: true, length: { maximum: 255 }
-  validates :last_name, presence: true, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 255 }
 end
