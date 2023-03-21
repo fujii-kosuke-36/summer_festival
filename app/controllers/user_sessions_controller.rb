12aba77950f130374f58bab3fd0a12af11f7f5cc
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-03-22 00:23:19 +0900

    #10 ユーザー関係修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,8 +4,9 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_to root_path, success: 'Login successful.'
+      redirect_to root_path, notice: 'ログインしました'
     else
+      flash.now[:alert] = "ログインに失敗しました!!"
       render :new
     end
   end
@@ -15,15 +16,14 @@ class UserSessionsController < ApplicationController
     redirect_to login_path, status: :see_other 
   end
 
-  # def guest_login
-  #   @guest_user = User.create(
-  #   name: 'ゲスト',
-  #   age: '**',
-  #   email: SecureRandom.alphanumeric(10) + "@email.com",
-  #   password: 'password',
-  #   password_confirmation: 'password'
-  #   )
-  #   auto_login(@guest_user)
-  #   redirect_to root_path, success: 'ゲストとしてログインしました'
-  # end
+  def guest_login
+    @guest_user = User.create(
+    name: 'ゲスト',
+    email: SecureRandom.alphanumeric(10) + "@email.com",
+    password: 'password',
+    password_confirmation: 'password'
+    )
+    auto_login(@guest_user)
+    redirect_to root_path
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
