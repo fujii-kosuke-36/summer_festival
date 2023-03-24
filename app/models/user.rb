d5cd34162160ce7e09f398dd7666be3854ff562b
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-03-24 21:58:14 +0900

    #10 リファクタ

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, presence: true
   validates :name, presence: true, length: { maximum: 255 }
+
+  def own?(object)
+    id == object.user_id
+  end
 end
