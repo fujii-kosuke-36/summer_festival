e801c212b0e0dc075c982f6480090a5b604a7e41
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-04-24 22:34:07 +0900

    #21 Fix:ブックマーク

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,14 +8,14 @@ class BookmarksController < ApplicationController
     @answer = Answer.find(params[:answer_id])
     current_user.bookmark(@answer)
     #render turbo_stream: turbo_stream.replace("bookmark-button-#{@answer.id}")
-    render turbo_stream: turbo_stream.replace("bookmark-button-#{@answer.id}", partial: 'bookmark/unbookmark', locals: { answer: @answer })
+    render turbo_stream: turbo_stream.replace("bookmark-button-#{@answer.id}", partial: 'bookmarks/unbookmark', locals: { answer: @answer })
   end
   
   def destroy
     @answer = current_user.bookmarks.find(params[:id]).answer
     current_user.unbookmark(@answer)
     #render turbo_stream: turbo_stream.replace("bookmark-button-#{@answer.id}")
-    render turbo_stream: turbo_stream.replace("bookmark-button-#{@answer.id}", partial: 'bookmark/bookmark', locals: { answer: @answer })
+    render turbo_stream: turbo_stream.replace("bookmark-button-#{@answer.id}", partial: 'bookmarks/bookmark', locals: { answer: @answer })
   end
  
 end
