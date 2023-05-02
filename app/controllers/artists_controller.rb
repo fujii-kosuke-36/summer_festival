8bbeb95701a9ab14bc2e77367b619f2ae56337e5
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-05-02 22:46:45 +0900

    #25 Fix:controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,5 @@
 class ArtistsController < ApplicationController
-
+  before_action :require_login, except: [:index]
   def index
     @q = Artist.ransack(params[:q])
     @artists = @q.result(distinct: true).order(created_at: :asc).page(params[:page])
@@ -9,7 +9,5 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find(params[:id])
     @answers = @artist.answers.includes(:user).order(created_at: :desc)
-    # tracks = RSpotify::Track.search(@artist.artist_name).first(10)
-    # @track = tracks.find { |track| track.artists[0].name == @artist.artist_name }
   end
 end
