1b481c31fe08f134cfd944de8a3100ee80a5efe3
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-05-07 13:02:42 +0900

    #25 空白の削除

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,7 +3,6 @@ class ArtistsController < ApplicationController
   def index
     @q = Artist.ransack(params[:q])
     @artists = @q.result(distinct: true).order(created_at: :asc).page(params[:page])
-    @url = Artist.all
   end
 
   def show
