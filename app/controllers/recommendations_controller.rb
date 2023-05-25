6043260b135785b071d93ba6ff79d0fcc5fedb45
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-05-25 09:26:13 +0900

    #31 おすすめ曲の変更

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,6 +1,6 @@
 class RecommendationsController < ApplicationController
     def index
-        recommendations = RSpotify::Recommendations.generate(seed_artists: ['3GP0RQRnBvpYVgdVYfcaGS'], seed_genres: ['post-teen-pop'], seed_tracks: ['73GtVPSJZIO8ABP7MeXFHk'], target_energy: 0.95, limit: 12, market: 'JP')
+        recommendations = RSpotify::Recommendations.generate(seed_artists: ['0SMhG4gXGD4gzLMMz08cQU'], seed_genres: ['post-teen pop'], target_energy: 0.85, limit: 12, market: 'JP')
         @tracks = recommendations.tracks.map { |track| { name: track.name, artist: track.artists.first.name, preview_url: track.preview_url, album_image_url: track.album.images.first["url"] } }
     end
       
