30391933a5f5731207cd347ccd15833b100457ff
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-05-26 15:28:14 +0900

    #31 フッター追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,16 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+  get 'terms', to: 'static_pages#terms'
+  get 'privacy', to: 'static_pages#privacy'
 
   post '/guest_login', to: 'user_sessions#guest_login'
-  
 
   resources :recommendations, only: %i[index]
   resources :festivals
   resources :users, only: %i[new create]
   resources :artists do
     resources :answers, only: %i[new create update destroy]
-  end  
+  end
   resources :bookmarks, only: %i[index create destroy]
 end
