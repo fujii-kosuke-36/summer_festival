a6bee510bd6d537f5f0773e2c54aa6a6929f2e94
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-05-30 18:14:58 +0900

    #31 recommend改良

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,4 +5,24 @@ class Artist < ApplicationRecord
     def self.ransackable_attributes(auth_object = nil)
         ["artist_id", "artist_image", "artist_name", "created_at", "id", "updated_at"]
     end
+    
+    def generate_recommendations
+    require 'rspotify'
+    ENV['ACCEPT_LANGUAGE'] = "ja"
+    RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
+    
+    artists = RSpotify::Artist.search(self.artist_name)
+    if artists.any?
+    seed_artist = artists.first
+
+    related_artists = seed_artist.related_artists
+    recommendations = related_artists.sample(5) # 5つのおすすめアーティストをランダムに選択
+
+    recommendations.each do |recommendation|
+        recommended_artist = OpenStruct.new(name: recommendation.name, spotify_id: recommendation.id)
+    end
+    end
+
+    end
+    
 end
