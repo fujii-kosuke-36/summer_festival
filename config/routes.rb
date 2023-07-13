d9187a4a0046f46b27a32dd22e0fb4bb3bb245c4
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-07-13 19:36:55 +0900

    Add:admin_controller #43

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,11 @@ Rails.application.routes.draw do
     resources :answers, only: %i[new create update destroy]
   end
   resources :bookmarks, only: %i[index create destroy]
+
+  namespace :admin do
+    root to: 'dashboards#index'
+    get 'login', to: 'user_sessions#new'
+    post 'login', to: 'user_sessions#create'
+    delete 'logout', to: 'user_sessions#destroy'
+  end
 end
