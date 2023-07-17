019ca67d1af1c0d5b2714b7b9e43d90be4b61a35
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-07-17 13:12:52 +0900

    Add:admin_title #43

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+    def page_title(page_title = '', admin = false)
+        base_title = if admin
+                     '(管理画面)'
+                     else
+                     'Fest-Hunt'
+                     end
+    
+        page_title.empty? ? base_title : page_title + ' | ' + base_title
+      end
+
 def default_meta_tags
     {
         site: 'Fest-Hunt',
