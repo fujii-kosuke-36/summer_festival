59502cdfc93d6342d958dd9522e8ab942b8b76a4
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-07-17 21:17:26 +0900

    翻訳ファイル　ルーティング #43

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,8 @@ Rails.application.routes.draw do
     get 'login', to: 'user_sessions#new'
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
+
+    resources :artists
+    resources :festivals
   end
 end
