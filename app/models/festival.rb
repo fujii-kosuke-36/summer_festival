7fbb0805f1a084e0ac9970e5068b6ea2e506b8ba
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-07-17 21:14:19 +0900

    中間テーブル作成 #43

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -1,5 +1,7 @@
 class Festival < ApplicationRecord
   has_many :artists
+  has_many :festival_artists
+  has_many :artists, through: :festival_artists
 
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "id", "location", "name", "prefecture", "region", "updated_at"]
