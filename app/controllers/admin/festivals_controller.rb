c89b1ec159712089bc0adf59cddb5ff5f4d650e3
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-07-19 21:18:36 +0900

    Fix:admin fes #43

--- app/controllers/admin/festivals_controller.rb
+++ app/controllers/admin/festivals_controller.rb
@@ -1,47 +1,32 @@
 class Admin::FestivalsController < Admin::BaseController
-    before_action :set_festival, only: %i[edit update destroy]
     def index
-      @festivals = Festival.all
+        @festivals = Festival.all
     end
-  
+
     def new
-      @festival = Festival.new
+        @festival = Festival.new
+        @artists = Artist.all
     end
-  
+    
     def create
-      @festival = Festival.new(festival_params)
-      if @festival.save
-        redirect_to admin_festivals_path, notice: 'フェスが追加されました。'
-      else
-        render :new
-      end
-    end
-
-    def edit
-    end
-
-    def update 
-        @festival.update(festival_params)
+        @festival = Festival.new(festival_params)
         if @festival.save
-            redirect_to admin_festivals_path, notice: 'フェスが更新されました。'
+        flash[:success] = "フェスを登録しました"
+        redirect_to root_url
         else
-            render :edit
+        flash[:danger] = "フェスが登録できませんでした"
+        render :new
         end
     end
 
-    def destroy
-        @festival.destroy
-        redirect_to admin_festivals_path, notice: 'フェスが削除されました。'
-    end
-  
-    private
-  
-    def festival_params
-        params.require(:festival).permit(:name, :prefecture, :location, :region, { :artist_ids => [] })
-    end
-
-    def set_festival
+    def show
         @festival = Festival.find(params[:id])
+        @artists = @festival.artists
+        end
+    
+    private
+    
+    def festival_params
+        params.require(:festival).permit(:name, :location, :prefecture, :region, artist_ids: [])
     end
-  end
-  
+    end
