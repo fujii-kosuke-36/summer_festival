17872b42a1082d0a59089bfc8ca5b1fd0eefb131
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-07-20 13:54:17 +0900

    add:関連するフェス #43

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,16 +1,10 @@
 class FestivalsController < ApplicationController
   def index
-    @q = Festival.ransack(params[:q])
-    @festivals = @q.result(distinct: true).order(created_at: :asc).page(params[:page])
+    @festivals = Festival.all
   end
 
   def show
     @festival = Festival.find(params[:id])
-    if @festival.id == 10
-      # アーティスト一覧を取得
-      @artists = Artist.all
-      # アーティストに紐づいた回答を取得（includesを使用してN+1問題を回避）
-      @answers = Answer.includes(:artist).where(artist_id: @artists.pluck(:id)).order(created_at: :desc)
-    end
+    @artists = @festival.artists
   end
 end
