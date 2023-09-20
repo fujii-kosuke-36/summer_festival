5bc4d242b7d94406d349f6d21d84b3744e362f78
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-09-21 01:20:54 +0900

    controller作成 #52

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
     has_many :group_users, dependent: :destroy
     belongs_to :owner, class_name: 'User'
-    has_many :users, through: :group_users
+    has_many :users, through: :group_users, source: :user
 
     validates :name, presence: true
     validates :introduction, length: { maximum: 200 }, presence: true
@@ -9,5 +9,9 @@ class Group < ApplicationRecord
 
     def is_owned_by?(user)
         owner.id == user.id
+    end
+
+    def includesUser?(user)
+        group_users.exists?(user_id: user.id)
     end
 end
