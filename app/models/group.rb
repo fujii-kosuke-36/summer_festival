b10d7f440cc83c25c37e671924f3f7abc3907569
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-09-20 17:09:40 +0900

    アソシエーション設定 #52

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,13 @@
 class Group < ApplicationRecord
+    has_many :group_users, dependent: :destroy
+    belongs_to :owner, class_name: 'User'
+    has_many :users, through: :group_users
+
+    validates :name, presence: true
+    validates :introduction, length: { maximum: 200 }, presence: true
+    has_one_attached :groupe_image
+
+    def is_owned_by?(user)
+        owner.id == user.id
+    end
 end
